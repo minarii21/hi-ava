9d5cd5eae0aa3147b12ebeda6d65da6c0a94750a
minarii21 <63478752+minarii21@users.noreply.github.com>
2020-05-23 23:46:56 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -1,10 +1,6 @@
 export default {
-  nicknames: ["Kamu", "Babe", "Sayang"],
+  
   greetings: {
-    evening: "Good Evening",
-    afternoon: "Good Afternoon",
-    day: "Good Day",
-    morning: "Good Morning",
-    night: "Good Night"
+    Happy Eid Mubarak
   }
 };
